bdbd00b9c78c6fa972a38fd4fcb78174b85171f6
Grishin Michael <69842449+JusticeShurup@users.noreply.github.com>
2024-05-11 16:35:08 +0500

    Added user service

---
+++ LiveChat.Application/Services/Interface/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using LiveChat.Application.Dtos;
+using LiveChat.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveChat.Application.Services.Interface
+{
+    public interface IUserService
+    {
+        User CreateUser(UserDto userDto);
+    }
+}
---
+++ LiveChat.Application/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using LiveChat.Application.Dtos;
+using LiveChat.Application.Repository;
+using LiveChat.Application.Services.Interface;
+using LiveChat.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveChat.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+     
+        public UserService(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public User CreateUser(UserDto userDto)
+        {
+            User user = new() 
+            { 
+                Id = userDto.Id,  
+                AvatarImage = userDto.AvatarImage,
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                CreatedDate = DateTime.UtcNow,
+                Messages = new List<Message>()
+            };
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.SaveChanges();
+            
+            return user;
+        }
+    }
+}
